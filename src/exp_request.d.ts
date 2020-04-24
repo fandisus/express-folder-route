198fed7b0289581c5087a62939bb21b987bda5bd
Fandi Susanto <great_beluluk@yahoo.com>
2020-04-24 23:16:12 +0700

    Added explanation about how to make the req.pathParams recognized by linters.

--- src/exp_request.d.ts
+++ src/exp_request.d.ts
@@ -3,3 +3,14 @@ declare namespace Express {
     pathParams?: string[]
   }
 }
+
+// Put the following lines into index.d.ts so the pathParams is recognized by linter
+// https://stackoverflow.com/questions/54030381/unable-to-extend-express-request-in-typescript/54030481
+// declare global {
+//   namespace Express {
+//     export interface Request {
+//       pathParams?: string[]
+//     }
+//   }
+// }
+
